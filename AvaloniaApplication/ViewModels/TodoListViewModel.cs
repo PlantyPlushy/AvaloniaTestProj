8df781ab886e584e07ee4c5eef9775de300d8044
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-04-14 10:22:07 -0400

    completed items delete from the list

--- AvaloniaApplication/ViewModels/TodoListViewModel.cs
+++ AvaloniaApplication/ViewModels/TodoListViewModel.cs
@@ -1,17 +1,39 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
 using AvaloniaApplication.Models;
+using ReactiveUI;
 
 namespace AvaloniaApplication.ViewModels
 {
     // Represents a collection of items that can update
     public class TodoListViewModel : ViewModelBase
     {
+        ObservableCollection<TodoItem> items;
         public TodoListViewModel(IEnumerable<TodoItem> items)
         {
             Items = new ObservableCollection<TodoItem>(items);
+
+            ShowUncompleted = ReactiveCommand.Create(() => 
+            {
+                ObservableCollection<TodoItem> filteredItems = new();
+                foreach (var item in Items)
+                {
+                    if (!item.IsChecked)
+                    {
+                        filteredItems.Add(new TodoItem { Description = item.Description });
+                    }
+                }
+                Items = filteredItems;
+            });
         }
 
-        public ObservableCollection<TodoItem> Items { get; }
+        public ObservableCollection<TodoItem> Items 
+        { 
+            get => items;
+            set => this.RaiseAndSetIfChanged(ref items, value);
+        }
+        public ReactiveCommand<Unit, Unit> ShowUncompleted { get; }
     }
 }
